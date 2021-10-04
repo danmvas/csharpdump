8fd3111ac9bfc0f03550d4c16be1774293c67ba3
Daniella Vasconcellos <daniellavasconc@gmail.com>
2021-10-04 15:22:38 -0300

    organização de pastas

---
+++ WebAPI/WebAPI/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string NameUser { get; set; }
+        public string Surname { get; set; }
+        public string Passport { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+
+    }
+}
---
+++ WebAPI/WebAPI/Services/CheckUsers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static WebAPI.Controllers.CrudController;
+
+namespace WebAPI.Services
+{
+    public class CheckUsers
+    {
+        public static bool ExistentUsers(long id)
+        {
+            return _users.Any(f => f.Id == id);
+        }
+    }
+}
