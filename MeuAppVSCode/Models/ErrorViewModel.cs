d0ea142e52afd7008cee078adc5f32bda1a572f3
Daniella Vasconcellos <daniellavasconc@gmail.com>
2021-10-04 15:32:55 -0300

    novo teste

---
+++ MeuAppVSCode/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MeuAppVSCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
