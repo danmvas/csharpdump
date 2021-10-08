122ca652bc23320b08be093a4d965f5472fd214d
Daniella Vasconcellos <daniellavasconc@gmail.com>
2021-10-08 02:48:30 -0300

    zzzzzzzzzzzzzzzzzzzz

---
+++ newAPI/newAPI/Controllers/CrudController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Model;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        public static List<User> _users = new List<User>();
+
+        // GET api/<CrudController>/5
+        [HttpGet("{id}")]
+        public User Get(string id)
+        {
+            return _users.Find(f => f.Id == id);
+
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _users;
+        }
+
+        // POST api/<CrudController>
+        [HttpPost]
+        public ActionResult Add(User user)
+        {
+            if (!CheckUsers.ExistentUsers(user.Id))
+            {
+                _users.Add(user);
+                SaveUsers.WriteBD();
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        // PUT api/<CrudController>/5
+        [HttpPut("{id}")]
+        public ActionResult Edit(int id, User users)
+        {
+            var userDb = _users.Find(f => f.Id == id);
+
+            if(userDb == null)
+            {
+                return BadRequest();
+            }
+
+            userDb.NameUser = users.NameUser;
+            userDb.Surname = users.Surname;
+            userDb.Passport = users.Passport;
+            userDb.Email = users.Email;
+            userDb.Phone = users.Phone;
+            userDb.Birthday = users.Birthday;
+
+            SaveUsers.WriteBD();
+
+            return Ok();
+
+        }
+
+        // DELETE api/<CrudController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+
+            var userDb = _users.Find(f => f.Id == id);
+
+            if (userDb == null)
+            {
+                return BadRequest();
+            }
+
+            _users.Remove(userDb);
+            SaveUsers.WriteBD();
+
+            return Ok();
+
+
+        }
+    }
+}
---
+++ newAPI/newAPI/Services/GetUsers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static WebAPI.Controllers.CrudController;
+using WebAPI.Model;
+using Newtonsoft.Json;
+
+namespace WebAPI.Services
+{
+    public class GetUsers
+    {
+        public static void GetBD()
+        {
+            var readBD = File.ReadAllLines("Services/db.txt");
+
+            foreach (var user in readBD)
+            {
+                var userGet = JsonConvert.DeserializeObject<User>(user);
+                _users.Add(userGet);
+            }
+            
+        }
+    }
+}
