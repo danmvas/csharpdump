559b327ea2814c8edb4c985227beb2202d1c7ea3
Daniella Vasconcellos <daniellavasconc@gmail.com>
2021-10-08 02:58:54 -0300

    no new minecraft merch?

---
+++ newAPI/newAPI/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data.Configurations;
+using WebAPI.Model;
+
+namespace WebAPI.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb;Initial Catalog=WebAPI;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfiguration).Assembly);
+        }
+    }
+}
---
+++ newAPI/newAPI/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Model;
+
+namespace WebAPI.Data.Configurations 
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.Property(f => f.Id).HasColumnType("VARCHAR(80)").IsRequired();
+            builder.Property(f => f.NameUser).HasColumnType("VARCHAR(80)").IsRequired();
+            builder.Property(f => f.Surname).HasColumnType("VARCHAR(80)").IsRequired();
+            builder.Property(f => f.Email).HasColumnType("VARCHAR(30)").IsRequired();
+            builder.Property(f => f.Passport).HasColumnType("VARCHAR(80)").IsRequired();
+            builder.Property(f => f.Phone).HasColumnType("VARCHAR(15)").IsRequired();
+            builder.Property(f => f.Birthday).HasColumnType("VARCHAR(12)").IsRequired();
+        }
+    }
+}
